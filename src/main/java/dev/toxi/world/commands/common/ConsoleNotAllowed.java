fd21f45e67d7e8851063a9d68205aa6c89f07263
Toxi Woxi <75816946+ToxiWoxi@users.noreply.github.com>
2023-11-26 05:13:09 -0500

    Add replacement for "No Console!"

---
+++ src/main/java/dev/toxi/world/commands/common/ConsoleNotAllowed.java
@@ -0,0 +1,15 @@
+package dev.toxi.world.commands.common;
+
+import dev.toxi.world.MyWorld;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ConsoleNotAllowed implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        MyWorld.getInstance().getServer().getConsoleSender().sendMessage("§4ERROR:§f This command cannot be used by the console!");
+        return false;
+    }
+
+}
