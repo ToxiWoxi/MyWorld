aa1a35343cdffececc5873815ad904f0e60b002e
Toxi Woxi <75816946+ToxiWoxi@users.noreply.github.com>
2023-11-28 04:44:40 -0500

    Add plugin tag to front of ConsoleNotAllowed msg

--- src/main/java/dev/toxi/world/commands/common/ConsoleNotAllowed.java
+++ src/main/java/dev/toxi/world/commands/common/ConsoleNotAllowed.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 public class ConsoleNotAllowed implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        MyWorld.getInstance().getServer().getConsoleSender().sendMessage("§4ERROR:§f This command cannot be used by the console!");
+        MyWorld.getInstance().getServer().getConsoleSender().sendMessage("§f[§aMyWorld§f] §4ERROR:§f This command cannot be used by the console!");
         return false;
     }
 
